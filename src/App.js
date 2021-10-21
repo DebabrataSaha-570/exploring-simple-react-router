2c9af4805a6f6e082dc4792e10f0ee07cd78f293
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-21 14:52:50 +0600

    simple router added

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import Home from './Components/Home/Home';
 import Friends from './Components/Friends/Friends';
 import About from './Components/About/About';
 import NotFound from './Components/NotFound/NotFound';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <div >
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
-      <NotFound></NotFound>
+
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+
+          </Route>
+          <Route path="/about">
+            <About></About>
+
+          </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
