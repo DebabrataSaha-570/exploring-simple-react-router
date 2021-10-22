fff148a75e04e6312a414ef1d1cf062872be1992
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-22 23:21:00 +0600

    update

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
     const { name, phone, website, address, id } = props.friend;
@@ -8,14 +8,26 @@ const Friend = (props) => {
         padding: '10px',
         borderRadius: '10px'
     }
+    let history = useHistory();
+    const handleFriendClick = () => {
+        history.push(`/friend/${id}`)
+    }
     return (
         <div style={friendStyle}>
-
             <h2>I am : {name} {id}</h2>
             <h5>Call me: {phone}</h5>
             <p>Visit me: {website}</p>
             <p>I live in {address.city}</p>
             <Link to={`/friend/${id}`}>Visit me</Link>
+            <br />
+            {/* bangla system  */}
+            <Link to={`/friend/${id}`}>
+                <button>Visit me</button>
+            </Link>
+            <br />
+            <br />
+            {/* proper way  */}
+            <button onClick={handleFriendClick}>Visit me 2</button>
         </div>
     );
 };
